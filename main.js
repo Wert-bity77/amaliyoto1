c423bbc328183443d3f7e1a799dfb64f80e0fa8a
Wert-bity77 <kabulovt707@gmail.com>
2024-12-15 22:40:20 +0500

    main.js va index.html
    Siuuuuu ronaldo zoroq messssi bot

---
+++ main.js
@@ -0,0 +1,7 @@
+const body = document.querySelector('body')
+const olim = document.querySelector('h1')
+let savol = prompt('backgroundColor')
+let savol2 = prompt('text color')
+
+body.style.backgroundColor = savol
+body.style.color = savol2
